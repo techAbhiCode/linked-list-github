bdf3a71e6e8da81aa7fc74683e04625b6182fcac
Shivam <shivamtech431@gmail.com>
2025-04-02 15:48:35 +0530

    This is the LinekdList program used to delete or remove from given last nth index

---
+++ deleteNthNodeFromEnd.java
@@ -0,0 +1,48 @@
+public class deleteNthNodeFromEnd {
+    public static void nthNodeDel(Node head, int n){
+        Node slow = head;
+        Node fast = head;
+
+        for (int i = 1; i <=n ; i++) {
+            fast = fast.next;
+        }
+        while (fast.next!=null){
+            slow =slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+    }
+    public static void display(Node head){
+        Node temp = head;
+        while (temp!=null){
+            System.out.print( temp.data+ " ");
+            temp = temp.next;
+        }
+    }
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static void main(String[] args) {
+        Node a = new Node(100);
+        Node b = new Node(13);
+        Node c = new Node(4);
+        Node d = new Node(5);
+        Node e = new Node(12);
+        Node f = new Node(10);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        display(a);
+        System.out.println();
+        nthNodeDel(a, 3);
+        System.out.println();
+        display(a);
+    }
+}
